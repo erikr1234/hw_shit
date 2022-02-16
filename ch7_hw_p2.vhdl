4bcc75de81846181239139fa523cab0680c98039
Erik Robles <90004893+erikr1234@users.noreply.github.com>
2022-02-16 11:54:44 -0500

    Update ch7_hw_p2.vhdl

--- ch7_hw_p2.vhdl
+++ ch7_hw_p2.vhdl
@@ -1,3 +1,26 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/16/2022 03:03:26 AM
+-- Design Name: 
+-- Module Name: beta2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 entity mux4t1 is --- ENTITY
 	port (  mux_A,mux_B,mux_C,mux_D  : in  std_logic_vector(7 downto 0);
 			M_SEL : in  std_logic_vector(1 downto 0);
@@ -5,16 +28,18 @@ entity mux4t1 is --- ENTITY
 end mux4t1;
 architecture my_mux of mux4t1 is --- ARCHITECTURE
 begin
-	with SEL select
-		M_OUT <= A when '00',
-				 B when '01',
-				 C when '10',
-				 D when '11',
+	with M_SEL select
+		M_OUT <= mux_A when "00",
+				 mux_B when "01",
+				 mux_C when "10",
+				 mux_D when "11",
 				 (others => '0') when others;
 end my_mux;
 -----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 entity reg8 is --- ENTITY
-	Port (  REG_IN  : in  std_logic_vector(7 downto 0);
+	port (  REG_IN  : in  std_logic_vector(7 downto 0);
 			LD,CLK  : in  std_logic;
 			REG_OUT : out std_logic_vector(7 downto 0));
 end reg8;
@@ -30,23 +55,33 @@ begin
 	end process;
 end reg8;
 ----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 entity dec1t2 is --- ENTITY
 	port (	D_SEL 			 : in  std_logic;
 			D_OUT_0, D_OUT_1 : out std_logic);
 end dec1t2;
 architecture my_dec of dec1t2 is --- ARCHITECTURE
 begin
-	with D_SEL select
-		D_OUT_0 <= 	'1' when '0',
-					'0' when '1',
-				 	(others => '0') when others;
-		D_OUT_1 <= 	'0' when '0',
-					'1' when '1',
-				 	(others => '0') when others;
-end dec1t2;
+    da_process: process (D_SEL) is
+    begin
+        if (D_SEL = '0') then
+           D_OUT_0 <= '1';
+           D_OUT_1 <= '0';
+        elsif (D_SEL = '1') then
+           D_OUT_0 <= '0';
+           D_OUT_1 <= '1';
+        else
+           D_OUT_0 <= '0';
+           D_OUT_1 <= '0';
+        end if;
+	end process da_process;
+end my_dec;
 ----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 entity bogus is
-	port (	X,Y,X 	: in std_logic_vector(7 downto 0);
+	port (	X,Y,Z 	: in std_logic_vector(7 downto 0);
 			DS 		: in std_logic;
 			CLK		: in std_logic;
 			MS 		: in std_logic_vector(1 downto 0);
@@ -70,30 +105,27 @@ architecture bogus_structural of bogus is
 	signal s_reg_A, s_reg_B, s_mux_result	: std_logic_vector(7 downto 0);
 	signal s_dec_0, s_dec_1 				: std_logic;
 begin
-	ra: reg8
+	reg_a: reg8
 	port map ( 	REG_IN 	=> s_mux_result,
-				LD 		=> LDA,
+				LD 		=> s_dec_0,
 				CLK 	=> CLK,
-				REG_OUT => RA );
-	rb: reg8
+				REG_OUT => s_reg_A );
+	reg_b: reg8
 	port map ( 	REG_IN 	=> RA,
-				LD 		=> LDA,
+				LD 		=> s_dec_1,
 				CLK 	=> CLK,
-				REG_OUT => RB );
+				REG_OUT => s_reg_B );
 	m1: mux4t1
-	port map (	mux_B 	=> Z,
+	port map (	mux_A   => s_reg_B,
+	            mux_B 	=> Z,
 				mux_C	=> Y,
 				mux_D	=> X,
 				M_SEL 	=> MS,
 				M_OUT 	=> s_mux_result);
 	d1: dec1t2
 	port map (	D_SEL 	=> DS,
-				CLK		=> CLK );
-	mux_A <= RB;
+	            D_OUT_0 => s_dec_0,
+	            D_OUT_1 => s_dec_1 );
+	RA <= s_reg_A;
+	RB <= s_reg_B;
 end architecture bogus_structural;
-
-
-
-
-
-
